f6c7bf27bc0cbb089374e485738bec5907c14bd2
Jamie McClymont <jamie.mcclymont@gmail.com>
2014-01-15 10:08:38 +1300

    Add error checking, fade in

--- client/script.js
+++ client/script.js
@@ -1,18 +1,26 @@
 var ws = new WebSocket("ws://10.22.33.231:8080");
 
-$(".gamebutton").bind("touchstart", function(e) {
-	$(this).addClass("pressed");
-	e.preventDefault();
+ws.onopen = function() {
+	$("body").fadeIn();
+	
+	$(".gamebutton").bind("touchstart", function(e) {
+		$(this).addClass("pressed");
+		e.preventDefault();
 
-	// Get the byte to send
-	var code = $(this).data("byte");
-	ws.send("#" + code + "1");
-})
+		// Get the byte to send
+		var code = $(this).data("byte");
+		ws.send("#" + code + "1");
+	})
 
-$(".gamebutton").bind("touchend", function(e) {
-	$(this).removeClass("pressed");
+	$(".gamebutton").bind("touchend", function(e) {
+		$(this).removeClass("pressed");
 
-	// Get the byte to send
-	var code = $(this).data("byte");
-	ws.send("#" + code + "0");
-})
+		// Get the byte to send
+		var code = $(this).data("byte");
+		ws.send("#" + code + "0");
+	})
+}
+
+ws.onerror = function() {
+	alert("An error occoured connecting to the websocket server");
+}
